0dad9c3d466ba1b50efabf175c0ed66a32e79ec0
willgeller <willgeller@de786789-df68-4216-9676-ab9cd87cb51e>
2016-01-19 18:57:26 +0000

    testing recaptcha

--- html/submit-form.php
+++ html/submit-form.php
@@ -1,31 +1,78 @@
 <?php
 
+ob_start();
+session_start();
 
-$name=$_POST['name'];
-$company=$_POST['company'];
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$message=$_POST['message'];
+  require_once('/recaptcha/recaptchalib.php');
+
+         //Recaptcha Settings
+       $publickey = "6Lc9yRETAAAAAPpOkZqZWsK6R7os4ogRJvUvCeUJ"; // you got this from the signup page
+    $privatekey = "6Lc9yRETAAAAALSH9b-MC-I0oQySQbogMXQHRzpa";
 
 
-$to='yourname@mail.com';
+//curl method posting
+//extract data from the post
+      extract($_POST);
 
-$headers = 'From: '.$name."\r\n" .
-	'Reply-To: '.$email."\r\n" .
-	'X-Mailer: PHP/' . phpversion();
-$subject = 'Message from Grepfrut';
-$body='You have got a new message from the contact form on your website - Grepfrut.'."\n\n";
+  if ($submit){
 
-$body.='Full name: '.$name."\n";
-$body.='Company name: '.$company."\n";
-$body.='Telephone Number: '.$phone."\n";
-$body.='Email: '.$email."\n";
-$body.='Message: '."\n".$message."\n";
-	
-if(mail($to, $subject, $body, $headers)) {
-	die('Message sent.');
-} else {
-	die('Error: Mail failed');
-}
+  $ok = 1;
 
-?>
+  $resp = recaptcha_check_answer ($privatekey,
+   $_SERVER["REMOTE_ADDR"],
+   $_POST["recaptcha_challenge_field"],
+   $_POST["recaptcha_response_field"]);
+
+   if (!$resp->is_valid) {
+      $ok = 0;
+    echo("Verify is failing");
+  }
+if ($ok){
+  //set POST variables
+// CTR curl code to resubmit to Salesforce web to lead
+//set POST variables
+  echo("Verify is progressing 1");
+  $url = 'https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8';
+  $fields = array(
+     'oid'=>urlencode($oid),
+		 'rating'=>urlencode($rating),
+     'retURL'=>urlencode($retURL),
+     'first_name'=>urlencode($first_name),
+     'last_name'=>urlencode($last_name),
+     'company'=>urlencode($company),
+     'country'=>urlencode($country),
+     'email'=>urlencode($email),
+		 'phone'=>urlencode($phone),
+		 'description'=>urlencode($description),
+     '00N6100000Aw7Zm'=>urlencode($Form_Name),
+     'lead_source'=>urlencode($lead_source),
+
+
+    );
+
+  //url-ify the data for the POST
+  foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+  rtrim($fields_string,'&');
+
+  //print_r($fields_string);
+
+  //open connection
+  $ch = curl_init();
+
+  //set the url, number of POST vars, POST data
+  curl_setopt($ch,CURLOPT_URL,$url);
+  curl_setopt($ch,CURLOPT_POST,count($fields));
+  curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+
+  //execute post
+  $result = curl_exec($ch);
+
+  //close connection
+  curl_close($ch);
+
+   } //if $OK
+else{
+  die ("<p>The reCAPTCHA wasn't entered correctly. Go back and try it again.<br>" .
+         "(reCAPTCHA said: " . $resp->error . ")</p>"); }
+} //if submit
+  ?>
